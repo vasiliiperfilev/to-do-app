7ea5410efe7f2a99e6bb9ec373161b5669dc6509
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2022-01-27 12:06:14 -0800

    Add add/removeTodo functions

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,9 +13,26 @@ function createController(){
 
     function showActiveProject(containerElement) {
         const project = projectStructurer.activeProject;
-        const content = domInteracer.createProjectPage(project);
+        const projectPage = domInteracer.createProjectPage(project);
+        setProjectPageEventListeners(projectPage, project);
         containerElement.innerHTML = "";
-        containerElement.appendChild(content);
+        containerElement.appendChild(projectPage);
+    }
+
+    function setProjectPageEventListeners(projectPage, project){
+        const addTodoBtn = projectPage.querySelector(".add-btn.todo");
+        const addTodoPopup = projectPage.querySelector(".add-popup.todo");
+        addTodoBtn.addEventListener("click", openAddToListPopup.bind(null, addTodoPopup, addTodoBtn));
+
+        const closePopupBtn = projectPage.querySelector(".add-popup.todo .cancel-btn-popup");
+        closePopupBtn.addEventListener("click", closeAddToListPopup.bind(null, addTodoPopup, addTodoBtn));
+
+        const todoList = projectPage.querySelector(".list.todo ul");
+        const addTodoPopupBtn = projectPage.querySelector(".add-popup.todo .add-btn-popup");
+        addTodoPopupBtn.addEventListener("click", () => {
+            addTodo(addTodoPopup, todoList, project);
+            closePopupBtn.click();
+        });
     }
 
     function chooseProject(containerElement){
@@ -49,31 +66,41 @@ function createController(){
         const parameters = domInteracer.collectInput(inputForm);
         const project = createProject(parameters);
         if (!projectStructurer.add(project)) return;
-        const listElement = domInteracer.createListElement(project);
-        listElement.addEventListener("click", chooseProject.bind(listElement, projectContainer));
-        const removeIcon = listElement.querySelector(".remove-icon");
+        const projectElement = domInteracer.createListElement(project);
+        projectElement.addEventListener("click", chooseProject.bind(projectElement, projectContainer));
+        const removeIcon = projectElement.querySelector(".remove-icon");
         removeIcon.addEventListener("click", (event) => {
             event.stopPropagation();
             removeProject(project, projectList, projectContainer);
         });
-        projectList.appendChild(listElement);
+        projectList.appendChild(projectElement);
+    }
+
+    function removeTodo(todo, todoList){
+        const todoElement = todoList.querySelector(`.c${todo.noSpaceTitle}`);
+        todoList.removeChild(todoElement);
+        const project = projectStructurer.activeProject;
+        project.remove(todo);
     }
 
     function addTodo(inputForm, todoList, project){
         const parameters = domInteracer.collectInput(inputForm);
         const todo = createTodo(parameters);
-        project.add(todo);
+        if (!project.add(todo)) return;
         const todoElement = domInteracer.createListElement(todo);
+        const removeIcon = todoElement.querySelector(".remove-icon");
+        removeIcon.addEventListener("click", (event) => {
+            event.stopPropagation();
+            removeTodo(todo, todoList);
+        });
         todoList.appendChild(todoElement);
     }
-    //addTodo
-    //removeTodo
 
     return {
         chooseProject,
         openAddToListPopup,
         closeAddToListPopup,
-        addProject,
+        addProject
     };
 }
 
